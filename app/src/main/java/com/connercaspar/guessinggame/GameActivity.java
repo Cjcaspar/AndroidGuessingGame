c31c712a99017dfaed40d9b6e39119fbc7450a13
Conner Caspar <conner.caspar@gmail.com>
2018-03-13 14:45:41 -0400

    Created mainactivity view, and onclick for the start button.

--- app/src/main/java/com/connercaspar/guessinggame/GameActivity.java
+++ app/src/main/java/com/connercaspar/guessinggame/GameActivity.java
@@ -2,19 +2,12 @@ package com.connercaspar.guessinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void screen2(View view) {
         setContentView(R.layout.activity_game_screen);
     }
-
-
 }
